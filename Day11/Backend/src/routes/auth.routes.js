5acb824a9e3ea9727db3b8fa0238c14c67b7fe06
ShatadruDhar <shatadrudhar10c@gmail.com>
2026-05-07 20:51:12 +0530

    done-till-JWT

--- Day11/Backend/src/routes/auth.routes.js
+++ Day11/Backend/src/routes/auth.routes.js
@@ -1,12 +1,15 @@
 const express=require("express")
 const userModel=require("../models/users.models")
 const AuthRouter=express.Router()
-
+//express.Router() is done so that we can create apis outside app.js file
+const jwt=require("jsonwebtoken")
+//package required to create a token
 
 
 AuthRouter.post("/register",async (req,res)=>{
 const {name,email,password}=req.body
 const userAlreadyExists=userModel.findOne({email})
+//just to check we have another user with same email or not
 if(userAlreadyExists){
     return res.status(409).json({
         message:"User already exists with this email address"
@@ -15,10 +18,16 @@ if(userAlreadyExists){
 const user=await userModel.create({
     name,email,password
 })
+const token=jwt.sign({id:user._id,email:user.email},process.env.JWT_SECRET)
+//here we create a token and sign it with a JWT_SECRET code kept in .env to identify that this user data stored in the database was done by us
 
+
+res.token("jwt_token",token)
+//using this line we store the jwt token in the cookie storage so that when whenever the user does any request the database takes the jwt_token from cookie storage to identify the(Authenticate) the user who is making the request 
 res.status(201).json({
     message:"User registered successfully",
-    user
+    user,
+    token
 })
 })
 
